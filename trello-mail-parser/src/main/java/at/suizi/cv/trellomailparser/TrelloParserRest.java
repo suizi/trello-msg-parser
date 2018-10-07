0d7fc8c51ddfcdfb6872f2a383422afb3bfa8d7f
Thomas Sulzbacher <thomas.sulzbacher@me.com>
2018-10-07 21:06:30 +0200

    test

--- trello-mail-parser/src/main/java/at/suizi/cv/trellomailparser/TrelloParserRest.java
+++ trello-mail-parser/src/main/java/at/suizi/cv/trellomailparser/TrelloParserRest.java
@@ -2,26 +2,32 @@ package at.suizi.cv.trellomailparser;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrelloParserRest {
-	@RequestMapping(path="/parse", method=RequestMethod.POST)
+
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome";
+	}
+
+	@RequestMapping(path = "/parse", method = RequestMethod.POST)
 	public String format(@RequestBody String msg) {
 		return convertBodyToTrelloMessage(msg);
 	}
 
 	private String convertBodyToTrelloMessage(String msg) {
 		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < msg.length(); i++) {
+
+		for (int i = 0; i < msg.length(); i++) {
 			char c = msg.charAt(i);
-			if(c == '[') {
-				
-				if(i+1 < msg.length()) {
-					if(msg.charAt(i+1) == 'h') {
-						if(isImage(msg, i+1)) {
+			if (c == '[') {
+
+				if (i + 1 < msg.length()) {
+					if (msg.charAt(i + 1) == 'h') {
+						if (isImage(msg, i + 1)) {
 							sb.append("![Web-Image](");
 						} else {
 							sb.append("[Link](");
@@ -29,7 +35,7 @@ public class TrelloParserRest {
 						continue;
 					}
 				}
-			} else if(c == ']'){
+			} else if (c == ']') {
 				sb.append(")");
 				continue;
 			}
@@ -40,13 +46,13 @@ public class TrelloParserRest {
 
 	private boolean isImage(String msg, int index) {
 		String link = "";
-		for(int i = index ; i < msg.length(); i++) {
-			char c =msg.charAt(i);
-			if(c == ']') {
+		for (int i = index; i < msg.length(); i++) {
+			char c = msg.charAt(i);
+			if (c == ']') {
 				return false;
 			}
 			link += c;
-			if(link.endsWith(".jpg") || link.endsWith(".png")) {
+			if (link.endsWith(".jpg") || link.endsWith(".png")) {
 				return true;
 			}
 		}
