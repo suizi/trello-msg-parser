daf36d6385fc10687a314c4bdd8ab070fea99581
Thomas Sulzbacher <thomas.sulzbacher@me.com>
2018-10-07 21:10:04 +0200

    test2

--- trello-mail-parser/src/main/java/at/suizi/cv/trellomailparser/TrelloParserRest.java
+++ trello-mail-parser/src/main/java/at/suizi/cv/trellomailparser/TrelloParserRest.java
@@ -3,14 +3,16 @@ package at.suizi.cv.trellomailparser;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrelloParserRest {
 
 	@RequestMapping("/")
+	@ResponseBody
 	public String home() {
-		return "Welcome";
+		return "Welcome to my first heroku experiments";
 	}
 
 	@RequestMapping(path = "/parse", method = RequestMethod.POST)
